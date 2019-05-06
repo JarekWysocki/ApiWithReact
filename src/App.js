3a5e3097b4399c7d2e00c3acd7c71b2353f88516
Jarek Wysocki <jarwys@wp.pl>
2019-05-06 17:01:50 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    value: "",
+    books: []
+  };
+  handleChange = e => {
+    const value = e.target.value;
+    this.setState({
+      value
+    });
+  };
+  handleSearchBook = e => {
+    e.preventDefault();
+    if (this.state.value === "") return alert("search book");
+    const search = this.state.value;
+    const xhr = new XMLHttpRequest();
+    xhr.open(
+      "GET",
+      "https://www.googleapis.com/books/v1/volumes?q=" + search,
+      true
+    );
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        const books = JSON.parse(xhr.responseText);
+        const results = books.items;
+        console.log(results);
+        this.setState({ books: results });
+      }
+    };
+    xhr.send(null);
+    this.setState({
+      value: ""
+    });
+  };
+
+  render() {
+    const books = this.state.books.map(book => (
+      <div key={book.id}>
+        <h2>{book.volumeInfo.title}</h2>
+        <h3>{book.volumeInfo.authors}</h3>
+        <p>{book.volumeInfo.description}</p>
+        <img alt="none" src={book.volumeInfo.imageLinks.thumbnail} />
+      </div>
+    ));
+    return (
+      <div className="App">
+        <form>
+          <label htmlFor="book">
+            Search books :
+            <input
+              type="text"
+              id="book"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+          </label>
+          <button onClick={this.handleSearchBook}>Search</button>
+        </form>
+        {books}
+      </div>
+    );
+  }
 }
-
 export default App;
