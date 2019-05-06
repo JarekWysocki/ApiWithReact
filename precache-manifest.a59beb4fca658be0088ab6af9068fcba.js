635677c326b14c1fd5e50178ecd788dde5e07d94
Jarek Wysocki <jarwys@wp.pl>
2019-05-06 17:25:57 +0200

    Updates

---
+++ precache-manifest.a59beb4fca658be0088ab6af9068fcba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6c6f5209e1ad272c7fda1a4c883ba86",
+    "url": "/ApiWithReact/index.html"
+  },
+  {
+    "revision": "fcffe5ef3a3f1f1b38ca",
+    "url": "/ApiWithReact/static/css/main.4610d485.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/ApiWithReact/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "fcffe5ef3a3f1f1b38ca",
+    "url": "/ApiWithReact/static/js/main.012dfa91.chunk.js"
+  },
+  {
+    "revision": "f306169e31d3921b26fc",
+    "url": "/ApiWithReact/static/js/runtime~main.1f15475f.js"
+  }
+]);
